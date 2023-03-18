0426e7d38f78aa275aef5e45efa8397b87dd2b73
MapOMatic <mapomatic.waze@gmail.com>
2023-03-18 18:06:59 -0400

    enable GLE devMode

--- WMEPIE-gle-test.user.js
+++ WMEPIE-gle-test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Place Interface Enhancements (GLE test)
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      2023.03.18.02
+// @version      2023.03.18.03
 // @description  Enhancements to various Place interfaces
 // @include      https://www.waze.com/editor*
 // @include      https://www.waze.com/*/editor*
@@ -118,7 +118,7 @@ var UpdateObject, MultiAction;
 
     async function init(){
         loadTranslations();
-        GLE = new GoogleLinkEnhancer();
+        GLE = new GoogleLinkEnhancer(true);
         hoursparser = new HoursParser();
 
         var $section = $("<div>", {style:"padding:8px 16px", id:"WMEPIESettings"});
